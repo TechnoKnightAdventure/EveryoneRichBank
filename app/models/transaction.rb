9a69c9651c46cc7e24df09b0dbb6fe04583f4b40
NonLogicalDev <utkin.oleg@me.com>
2014-12-01 19:07:20 -0800

    Adding the transaction model

---
+++ app/models/transaction.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id                 :integer          not null, primary key
+#  payment_account_id :integer
+#  actor_id           :integer
+#  description        :string(255)
+#  type               :string(255)
+#  amount             :decimal(, )
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
+class Transaction < ActiveRecord::Base
+end
---
+++ db/migrate/20141201201132_create_transactions.rb
@@ -0,0 +1,14 @@
+class CreateTransactions < ActiveRecord::Migration
+  def change
+    create_table :transactions do |t|
+      t.belongs_to :payment_account
+
+      t.integer  :actor_id
+      t.string   :description
+      t.string   :type
+      t.decimal  :amount
+
+      t.timestamps
+    end
+  end
+end
