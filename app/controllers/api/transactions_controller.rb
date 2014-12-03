15bca223ceed26c61d166e84e2b0241c133f6a50
NonLogicalDev <utkin.oleg@me.com>
2014-12-02 20:49:31 -0800

    Wiring the transactions to the Customer UI

---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,26 @@
+class Api::TransactionsController < Api::ApiResource
+
+  def index
+    raise ArgumentError, "Payment Account id is missng" if params[:payment_account_id].nil?
+
+    begin
+      if current_user.role == 'customer' 
+        customer = current_user
+      elsif 
+        customer = User.find(params[:customer_id])
+      end
+
+      account      = customer.payment_account.find(params[:payment_account_id])
+      transactions = account.transaction_log.all
+    rescue Exception => e
+      logger.fatal("Can not ger transaction '#{e.message}'")
+      raise_app_error!
+    end
+
+    _render({
+      transactions: transactions.as_json(
+        only: [:amount, :description, :trans_type, :updated_at]
+      )
+    })
+  end
+end
